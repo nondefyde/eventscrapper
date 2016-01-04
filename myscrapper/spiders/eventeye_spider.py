cd864d4772150e72e17ea03fad4b90ccfedb1154
Emmanuel Okafor <nondefyde@gmail.com>
2016-01-04 22:47:08 +0100

    first commit to online repo

--- myscrapper/spiders/eventeye_spider.py
+++ myscrapper/spiders/eventeye_spider.py
@@ -12,27 +12,37 @@ class EventEyeSpider(scrapy.Spider):
 
     name = "eventeye"
     allowed_domains = ["eventseye.com"]
-    start_urls = ['http://www.eventseye.com/fairs/Upcoming_Trade_Shows.html']
+    # start_urls = ['http://www.eventseye.com/fairs/Upcoming_Trade_Shows.html']
+    start_urls = ['http://www.eventseye.com/fairs/d1_trade-shows_january_2016.html']
+    pagination_url = 'http://www.eventseye.com/fairs/d1_trade-shows_january_2016_%s.html'
+    pag_max_count = 13
+
     # start_urls = []
 
     def __init__(self, **kwargs):
         super(EventEyeSpider, self).__init__(**kwargs)
         self.init_Month()
-
-        # self.urls = self.get_dict('assets\event_eye_start_urls.json')
-        # for url in self.urls:
-        #     self.start_urls = self.start_urls + [url['url']]
-
+        # self.url_per_month()
         self.google_maps = GoogleMaps(api_key='AIzaSyCaxLzZ2r7AbCJiIy5RtJ4jOQXcOlDbeV0')
 
     def parse(self, response):
-        urls = response.css('table td.mtb a::attr(href)').extract()
-        for url in urls:
+
+        urls = response.css('table table tr[bgcolor] td.mt a::attr(href)').extract()
+        dates = response.css('table table tr[bgcolor] td:first-child::text').extract()
+        for num in range(len(urls)):
+            url = urls[num]
+            raw_date = dates[num]
             item = EventItem()
-            item['url'] = 'http://www.eventseye.com'+url
-            print url
+            item['url'] = 'http://www.eventseye.com/fairs/' + url.strip()
+            item['date'] = raw_date.strip()
+            self.get_event_date(item)
             request = scrapy.Request(item['url'], callback=self.parse_event, meta={'item': item})
             yield request
+
+    def url_per_month(self):
+        for page_num in range(1, self.pag_max_count):
+            url = self.pagination_url % page_num
+            self.start_urls = self.start_urls + [url]
 
     def print_start_urls(self, response):
         urls = response.css('table td.mtb a::attr(href)').extract()
@@ -43,9 +53,8 @@ class EventEyeSpider(scrapy.Spider):
 
     def parse_event(self, response):
         sel = response.css('html')
-        item = EventItem()
+        item = response.meta['item']
         item['event_website'] = 'http://www.eventseye.com/'
-        item['url'] = response.request.url
         item['source'] = sel.css('table td.mt a[rel="nofollow"]::attr(href)').extract()[0].strip()
         item['title'] = sel.css('table table h1 b::text').extract()[0]
         item['description'] = ''
@@ -53,17 +62,13 @@ class EventEyeSpider(scrapy.Spider):
         item['price_range'] = ''
         item['addressLocality'] = sel.css('table a font.etb::text').extract()[0]
         item['location'] = sel.css('table table h1 b::text').extract()[0]
-        item['date'] = sel.css('table td.mt:first-child span::text').extract()[0].strip()
         state_code = sel.css('table td.mt span a::attr(href)').extract()[0].strip()
 
-        state_code = (state_code[state_code.rindex('-')+1:state_code.index('.')]).upper()
+        state_code = (state_code[state_code.rindex('-') + 1:state_code.index('.')]).upper()
         item['state_code'] = state_code
 
         item['category'] = 'Cant Figure this out'
         item['industry'] = 'Cant Figure this out'
-
-
-        self.get_datetime(item)
 
         loc = self.google_maps.search(location=item['addressLocality'])
         my_location = loc.first()
@@ -91,6 +96,34 @@ class EventEyeSpider(scrapy.Spider):
             data = json.load(f)
         return data
 
+    def get_event_date(self, item):
+        item['event_start'] = '0000-00-00 00:00:0'
+        item['event_end'] = '0000-00-00 00:00:0'
+        raw_date = item['date']
+        try:
+            raw_date = raw_date.split(' ')
+            from_day = ((raw_date[0]).split('.'))[0]
+            to_day = ((raw_date[2]).split('.'))[0]
+
+            from_month = ((raw_date[0]).split('.'))[1]
+            to_month = ((raw_date[2]).split('.'))[1]
+
+            year = raw_date[3]
+
+            from_date = year + '-' + from_month + '-' + from_day + 'T10:00:00'
+            to_date = year + '-' + to_month + '-' + to_day + 'T10:00:00'
+
+            event_start = datetime.strptime(from_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
+            event_end = datetime.strptime(to_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
+
+            item['event_start'] = event_start
+            item['event_end'] = event_end
+
+        except Exception as e:
+            item['event_start'] = '0000-00-00 00:00:0'
+            item['event_end'] = '0000-00-00 00:00:0'
+        pass
+
     def init_Month(self):
         self.month = {'Jan': '01', 'Feb': '02', 'March': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jly': '07',
                       'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
@@ -105,19 +138,19 @@ class EventEyeSpider(scrapy.Spider):
                 dot_index = date.index(".")
                 comma_index = date.index(",")
 
-                days = date[dot_index+1:comma_index]
+                days = date[dot_index + 1:comma_index]
                 days = days.split('-')
 
                 from_day = (days[0]).strip()
                 to_day = (days[1]).strip()
 
-                year = (date[comma_index+1:]).strip()
+                year = (date[comma_index + 1:]).strip()
                 month_str = (date[0:dot_index]).strip()
 
                 month = self.month[month_str]
 
-                from_date = year+'-'+month+'-'+from_day+'T10:00:00'
-                to_date = year+'-'+month+'-'+to_day+'T10:00:00'
+                from_date = year + '-' + month + '-' + from_day + 'T10:00:00'
+                to_date = year + '-' + month + '-' + to_day + 'T10:00:00'
 
                 event_start = datetime.strptime(from_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
                 event_end = datetime.strptime(to_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
@@ -128,7 +161,7 @@ class EventEyeSpider(scrapy.Spider):
             else:
                 comma_index = date.index(",")
 
-                year = (date[comma_index+1:]).strip()
+                year = (date[comma_index + 1:]).strip()
 
                 days = date.split('-')
                 from_month_str = (days[0].strip())[0:3]
@@ -136,7 +169,7 @@ class EventEyeSpider(scrapy.Spider):
 
                 for_days = date.split(',')
                 for_days = for_days[0]
-                for_days = for_days[for_days.index('.')+1:]
+                for_days = for_days[for_days.index('.') + 1:]
                 for_days = for_days.split(to_month_str)
 
                 from_day = (for_days[0]).strip()
@@ -148,8 +181,8 @@ class EventEyeSpider(scrapy.Spider):
                 from_month = self.month[from_month_str]
                 to_month = self.month[to_month_str]
 
-                from_date = year+'-'+from_month+'-'+from_day+'T10:00:00'
-                to_date = year+'-'+to_month+'-'+to_day+'T10:00:00'
+                from_date = year + '-' + from_month + '-' + from_day + 'T10:00:00'
+                to_date = year + '-' + to_month + '-' + to_day + 'T10:00:00'
 
                 event_start = datetime.strptime(from_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
                 event_end = datetime.strptime(to_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
